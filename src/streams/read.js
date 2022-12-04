34a2dbacb82c2582e70cf33325ce536475344bda
Stan Effy <holydonk@gmail.com>
2022-12-04 22:09:44 +0200

    read stream done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+    const readerStream = fs.createReadStream('./src/streams/files/fileToRead.txt');
+    readerStream.setEncoding('UTF8');
+
+    let data = "";
+    readerStream.on('data', function(chunk) {
+        data += chunk;
+    });
+
+    readerStream.on('end',function() {
+        console.log(data);
+    });
+
+    readerStream.on('error', function(err) {
+        console.log(err.stack);
+    });
 };
 
 await read();
