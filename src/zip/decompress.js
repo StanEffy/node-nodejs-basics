6093fbe6c83a62bfbb2aba8a14bfe4176af40050
Stan Effy <holydonk@gmail.com>
2022-12-05 21:41:34 +0200

    decompress done

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import fs from "fs";
+import * as zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+    const gunzipStream = zlib.createGunzip();
+
+    const readStream = fs.createReadStream("./src/zip/files/archive.gz");
+    const writeStream = fs.createWriteStream("./src/zip/files/fileToCompress.txt");
+
+    readStream.pipe(gunzipStream).pipe(writeStream);
 };
 
 await decompress();
