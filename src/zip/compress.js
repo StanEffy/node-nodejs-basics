93a794be642263e0e5d91e2abded2c046025f728
Stan Effy <holydonk@gmail.com>
2022-12-05 21:39:17 +0200

    zip compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import * as zlib from "zlib";
+import fs from "fs";
+
 const compress = async () => {
-    // Write your code here 
+    const gzipStream = zlib.createGzip();
+
+    const readStream = fs.createReadStream("./src/zip/files/fileToCompress.txt");
+    const writeStream = fs.createWriteStream("./src/zip/files/archive.gz");
+
+    readStream.pipe(gzipStream).pipe(writeStream);
 };
 
 await compress();
