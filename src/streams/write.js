97670a5c050ed3e4e996474753293082c010f3a9
Stan Effy <holydonk@gmail.com>
2022-12-05 21:34:36 +0200

    streams done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,8 @@
+import fs from "fs";
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = fs.createWriteStream('./src/streams/files/fileToWrite.txt');
+    process.stdin.pipe(writeStream);
 };
 
 await write();
